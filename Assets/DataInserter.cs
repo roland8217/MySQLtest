de26a5e5e0615ccbbcf42c88025ca1e4a293b7a2
roland8217 <33519462+roland8217@users.noreply.github.com>
2017-11-11 16:54:55 -0500

    firstpush

---
+++ Assets/DataInserter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataInserter : MonoBehaviour {
+
+	public string inputUserName;
+	public string inputPassword;
+	public string inputEmail;
+
+	string CreateUserURL = "http://demonblaster.000webhostapp.com/InsertUser.php";
+
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space)) CreateUser(inputUserName, inputPassword, inputEmail);
+	}
+
+	public void CreateUser(string username, string password, string email)
+	{
+		WWWForm form = new WWWForm();
+		form.AddField("usernamePost", username);
+		form.AddField("passwordPost", password);
+		form.AddField("emailPost", email);
+		WWW www = new WWW(CreateUserURL, form);
+	}
+}
---
+++ Assets/Login.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour {
+
+	public string inputUserName;
+	public string inputPassword;
+
+	string LoginURL = "http://demonblaster.000webhostapp.com/Login.php";
+
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.L)) StartCoroutine(LoginToDB(inputUserName, inputPassword));
+	}
+
+	IEnumerator LoginToDB(string username, string password)
+	{
+		WWWForm form = new WWWForm();
+		form.AddField("usernamePost", username);
+		form.AddField("passwordPost", password);
+
+		WWW www = new WWW(LoginURL, form);
+		yield return www;
+		print(www.text);
+	}
+}
